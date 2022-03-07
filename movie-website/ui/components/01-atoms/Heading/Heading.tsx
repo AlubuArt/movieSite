adfbf8569c2e029bedbaa86bcc87fe2eef2beee8
AlubuArt <jc@jcvisueldesign.dk>
2022-03-07 19:08:38 +0100

    Added ATOM - headings

---
+++ movie-website/ui/components/01-atoms/Heading/Heading.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classNames from "classnames";
+import styles from './Heading.module.css';
+
+interface HeadingProps {
+    children: React.ReactNode;
+    type: 'h1' | 'h2' | 'h3';
+    tag: 'h1' | 'h2' | 'h3';
+
+}
+
+
+export const HeadingProps: React.FC<HeadingProps> = ({children, tag, type}) => 
+React.createElement(
+    tag, 
+    {className: classNames(styles.Heading, styles[`Heading___${type}`])},
+    children
+)
