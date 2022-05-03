32e986cfdeaaf1d5355edb39235bf3f8e1b074cd
AlubuArt <jc@jcvisueldesign.dk>
2022-05-03 07:03:23 +0200

    added prettier config file

--- movie-website/ui/components/03-organisms/Hero/Hero.tsx
+++ movie-website/ui/components/03-organisms/Hero/Hero.tsx
@@ -1,23 +1,66 @@
-import React from "react";
-import styles from "./Hero.module.css";
-import { MovieInfoCard } from "../../02-molecules/MovieInfoCard/MovieInfoCard";
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './Hero.module.css';
+import { MovieInfoCard } from '../../02-molecules/MovieInfoCard/MovieInfoCard';
+import { Modal } from '../Modal/Modal';
 
 interface HeroProps {
-    movie: Models.MovieItem;
+  movie: Models.MovieItem;
 }
 
 export const Hero: React.FC<HeroProps> = ({ movie }) => {
-    return (
-        <div className={styles.wrapper}>
-            <img
-                className={styles.Backdrop}
-                src={
-                    movie.plprogram$thumbnails["orig-720x1280"]?.plprogram$url
-                }
-            />
-            <div className={styles.MovieInfoCardWrapper}>
-                <MovieInfoCard movie={movie} />
-            </div>
+  const [showModal, setShowModal] = useState<boolean>(false);
+  //Create a ref to pass the modal div
+  const modalRef = useRef();
+  useOnClickOutside(modalRef, () => setShowModal(false));
+
+  const handleModal = () => {
+    setShowModal(true);
+  };
+  return (
+    <div className={styles.wrapper}>
+      <img
+        className={styles.Backdrop}
+        src={movie.plprogram$thumbnails['orig-720x1280']?.plprogram$url}
+      />
+      <div className={styles.MovieInfoCardWrapper}>
+        <MovieInfoCard movie={movie} handleModal={handleModal} />
+      </div>
+      {showModal ? (
+        <div className={styles.ModalWrapper}>
+          <div ref={modalRef}>
+            <Modal isOpen={showModal} movie={movie} />
+          </div>
         </div>
-    );
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
+
+function useOnClickOutside(ref, handler) {
+  useEffect(
+    () => {
+      const listener = (event) => {
+        // Do nothing if clicking ref's element or descendent elements
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+        handler(event);
+      };
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
+      return () => {
+        document.removeEventListener('mousedown', listener);
+        document.removeEventListener('touchstart', listener);
+      };
+    },
+    // Add ref and handler to effect dependencies
+    // It's worth noting that because passed in handler is a new ...
+    // ... function on every render that will cause this effect ...
+    // ... callback/cleanup to run every render. It's not a big deal ...
+    // ... but to optimize you can wrap handler in useCallback before ...
+    // ... passing it into this hook.
+    [ref, handler]
+  );
+}
--- movie-website/ui/components/03-organisms/Modal/Modal.tsx
+++ movie-website/ui/components/03-organisms/Modal/Modal.tsx
@@ -1,66 +1,62 @@
-import React, { useEffect } from "react";
-import { Heading } from "../../01-atoms/Heading";
-import { Paragraph } from "../../01-atoms/Paragraph/Paragraph";
-import styles from "./Modal.module.css";
+import React, { useEffect } from 'react';
+import { Heading } from '../../01-atoms/Heading';
+import { Paragraph } from '../../01-atoms/Paragraph/Paragraph';
+import styles from './Modal.module.css';
 
 export interface ModalProps {
-    isOpen: boolean;
-    movie: Models.MovieItem;
+  isOpen: boolean;
+  movie: Models.MovieItem;
 }
 
 export const Modal: React.FC<ModalProps> = ({ isOpen, movie }) => {
-    const wrappedItems = (title) => {
-        return movie.plprogram$credits
-            .filter((item: any) => item.plprogram$creditType === title)
-            .map((item: any) => {
-                return (
-                    <p className={styles.ListItems}>
-                        {item.plprogram$personName}
-                    </p>
-                );
-            });
-    };
-    return (
-        <div className={styles.ModalWrapper}>
-            <div className={styles.ModalInfo}>
-                <div className={styles.ModalInfoLeft}>
-                    <div className={styles.ModalTitle}>
-                        <Heading tag={"h2"} type={"h2"}>
-                            {movie.title}
-                        </Heading>
-                    </div>
-                    <div className={styles.ModalDescription}>
-                        <Paragraph tag={"p"}>
-                            {movie.plprogram$descriptionLocalized.en}
-                        </Paragraph>
-                    </div>
-                </div>
-                <div className={styles.ModalInfoRight}>
-                    <div className={styles.Director}>
-                        <Heading tag={"h3"} type={"h3"}>
-                            Director
-                        </Heading>
-                        {wrappedItems("director")}
-                    </div>
-                    <div className={styles.Actors}>
-                        <Heading tag={"h3"} type={"h3"}>
-                            Actors
-                        </Heading>
-                        {wrappedItems("actor")}
-                    </div>
-                </div>
-            </div>
-            <div className={styles.ModalEmbedVideoPlayer}>
-                <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${movie.tdc$youtubeTrailer}`}
-                    title={movie.title}
-                    frameBorder="1"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
-            </div>
+  const wrappedItems = (title) => {
+    return movie.plprogram$credits
+      .filter((item: any) => item.plprogram$creditType === title)
+      .map((item: any) => {
+        return <p className={styles.ListItems}>{item.plprogram$personName}</p>;
+      });
+  };
+  return (
+    <div className={styles.ModalWrapper}>
+      <div className={styles.ModalInfo}>
+        <div className={styles.ModalInfoLeft}>
+          <div className={styles.ModalTitle}>
+            <Heading tag={'h2'} type={'h2'}>
+              {movie.title}
+            </Heading>
+          </div>
+          <div className={styles.ModalDescription}>
+            <Paragraph tag={'p'}>
+              {movie.plprogram$descriptionLocalized.en}
+            </Paragraph>
+          </div>
         </div>
-    );
+        <div className={styles.ModalInfoRight}>
+          <div className={styles.Director}>
+            <Heading tag={'h3'} type={'h3'}>
+              Director
+            </Heading>
+            {wrappedItems('director')}
+          </div>
+          <div className={styles.Actors}>
+            <Heading tag={'h3'} type={'h3'}>
+              Actors
+            </Heading>
+            {wrappedItems('actor')}
+          </div>
+        </div>
+      </div>
+      <div className={styles.ModalEmbedVideoPlayer}>
+        <iframe
+          width='560'
+          height='315'
+          src={`https://www.youtube.com/embed/${movie.tdc$youtubeTrailer}`}
+          title={movie.title}
+          frameBorder='1'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 };
